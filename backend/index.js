0de81f5a23be2109a792c11387b0e929a9353e02
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-15 00:00:26 -0300

    Add /helloworld route for API work testing

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.get('/helloworld', (_, res) => res.send('Hello World!'));
+
+app.listen(4000, () => console.log(`Server open on port ${PORT}`));
