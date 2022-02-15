e216b248c2e5644f05f99843219e5b4b1f215105
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-15 14:17:04 -0300

    Add test for successfully login

--- backend/__tests__/login.test.js
+++ backend/__tests__/login.test.js
@@ -1,5 +1,8 @@
 const frisby = require('frisby');
+const jwt = require('jsonwebtoken');
 const URL = 'http://localhost:4000/';
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 describe('Testing POST /api/login', () => {
   it('Verify if when called with malformed data, return status code 400 with JSON { message: "Campos inválidos" }', async () => {
@@ -11,5 +14,23 @@ describe('Testing POST /api/login', () => {
     })
       .expect('status', 400)
       .expect('json', { message: 'Campos inválidos' });
+  });
+
+  it('Verify if when inserted correct data, return a token with 16 random characters', async () => {
+    await frisby.post(`${URL}api/login`, {
+      body: {
+        email: 'example@example.com',
+        password: '123456'
+      }
+    })
+    .expect('status', 200)
+    .then((responseLogin) => {
+      const { body } = responseLogin;
+      const result = JSON.parse(body);
+      const { token } = result;
+
+      expect(token.length).toBe(16);
+      expect(jwt.verify(token, JWT_SECRET)).toBe(true)
+    });
   });
 });
