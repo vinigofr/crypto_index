606d67969350b9e707b0d2492372364e9ade285b
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-15 02:25:01 -0300

    Change guest response to JSON response

--- backend/Routes/helloWorldRoutes.js
+++ backend/Routes/helloWorldRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
 
-router.get('/', (_, res) => res.send('Hello World!'));
+router.get('/', (_, res) => res.json({ message: 'Hello World!' }));
 
 module.exports = router;
