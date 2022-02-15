00602620b18df09701ffa7629eb74c84cf12eaa5
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-15 00:21:45 -0300

    Add Router folder system

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const helloWorldRoutes = require('./Routes/helloWorldRoutes');
 
 const app = express();
-
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
-app.get('/helloworld', (_, res) => res.send('Hello World!'));
+app.use('/helloworld', helloWorldRoutes);
 
 app.listen(4000, () => console.log(`Server open on port ${PORT}`));
