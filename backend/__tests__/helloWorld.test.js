7cb1c364ada0c0642c35d3b96045917382ee0dae
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-15 02:25:24 -0300

    Add first test file to /helloworld route

---
+++ backend/__tests__/helloWorld.test.js
@@ -0,0 +1,13 @@
+const frisby = require('frisby');
+const URL = 'http://localhost:4000/';
+
+// References to learn testing
+// https://medium.com/serasa-consumidor/parte-1-introdu%C3%A7%C3%A3o-e-requisi%C3%A7%C3%B5es-get-6c7ec9c7c778
+// https://docs.frisbyjs.com/api-and-usage/expectations-assertions#expect-jsontypes-path-data
+describe('Testing "Hello World" route', () => {
+  it('Verify if when called endpoint /helloworld, returns "Hello World"', async () => {
+    await frisby.get(`${URL}helloworld`)
+      .expect('status', 200)
+      .expect('json', { message: 'Hello World!' });
+  });
+});
