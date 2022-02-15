79cf51d0b21b93acdc7aa2be6a847e804e99a66f
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-15 13:22:15 -0300

    Add loginValidation Middleware and some comments to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+
+// Routes
 const helloWorldRoutes = require('./Routes/helloWorldRoutes');
+const loginRoutes = require('./Routes/loginRoutes');
+
+// Middlewares
+const { loginValidation } = require('./middlewares/loginValidation');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -7,6 +13,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 
 app.use('/helloworld', helloWorldRoutes);
+app.use('/api/login', loginValidation, loginRoutes);
 
 app.listen(PORT, () => console.log(`Server open on port ${PORT}`));
 
