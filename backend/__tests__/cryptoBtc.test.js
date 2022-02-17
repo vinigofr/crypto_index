462dbd7715e67acaf4f263266a6706fd5f391a0e
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-17 09:18:08 -0300

    Add first server mock for testing

--- backend/__tests__/cryptoBtc.test.js
+++ backend/__tests__/cryptoBtc.test.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const frisby = require("frisby");
-const jwt = require("jsonwebtoken");
 const URL = "http://localhost:4000/";
+const fetchBtcCurrency = require('../api/fetchBtcCurrency');
+const { app } = require('../app');
 
-const API_MOCK_RESPONSE = {
+jest.mock('../api/fetchBtcCurrency');
+
+
+const API_RESPONSE = {
   time: {
     updated: "Feb 15, 2022 23:23:00 UTC",
     updatedISO: "2022-02-15T23:23:00+00:00",
@@ -28,24 +32,42 @@ const API_MOCK_RESPONSE = {
 };
 
 // For BRL
-const OneDollar_BRAZIL = 5.40 // Brazilian rate_float from file .JSON
-const OneBTC_Dollar = 44299.8351 // From API rate_float
-const OneBTC_REAIS = OneDollar_BRAZIL * OneBTC_Dollar; // 239219.10954
-
-// For CAD
-const OneDollar_CANADA = 5.40 // canadaian rate_float from file .JSON
-const OneBTC_Dollar = 44299.8351 // From API rate_float
-const OneBTC_CANADA = OneDollar_BRAZIL * OneBTC_Dollar; // 239219.10954
-
-// For EUR
-const OneDollar_EUROPA = 5.40 // EUROPA rate_float from file .JSON
-const OneBTC_Dollar = 44299.8351 // From API rate_float
-const OneBTC_EUROPA = OneDollar_BRAZIL * OneBTC_Dollar; // 239219.10954
+// const OneDollar_BRAZIL = 5.40 // Brazilian rate_float from file .JSON
+// const OneBTC_Dollar = 44299.8351 // From API rate_float
+// const OneBTC_REAIS = OneDollar_BRAZIL * OneBTC_Dollar; // 239219.10954
+// // For CAD
+// const OneDollar_CANADA = 5.40 // canadaian rate_float from file .JSON
+// const OneBTC_Dollar = 44299.8351 // From API rate_float
+// const OneBTC_CANADA = OneDollar_BRAZIL * OneBTC_Dollar; // 239219.10954
+// // For EUR
+// const OneDollar_EUROPA = 5.40 // EUROPA rate_float from file .JSON
+// const OneBTC_Dollar = 44299.8351 // From API rate_float
+// const OneBTC_EUROPA = OneDollar_BRAZIL * OneBTC_Dollar; // 239219.10954
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
 describe("Testing GET /api/crypto/btc", () => {
-  it('', async () => {});
-  it('', async () => {});
-  it('', async () => {});
+
+  beforeAll((done) => {
+    server = app.listen(4000, () => {
+      done();
+    });
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('Verify if returns correct response with status code 200', async () => {
+    
+    fetchBtcCurrency.getCurrency.mockImplementation(() => ({ a: 'b'}));
+
+    // const defaultExport = await fetchBtcCurrency();
+    // expect(defaultExport).toBe(JSON.stringify({ a: 'b'})); // This assert works
+
+    await frisby
+      .get(`${URL}api/crypto/btc`)
+      .expect('status', 200)
+      .expect('json', { a: 'b'}); // Integration test with Frisby does not work correctly.
+  });
 });
