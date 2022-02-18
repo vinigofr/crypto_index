c3ecc88630571cfa184203bf994b334e83422d58
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-17 23:08:24 -0300

    Delete Joi schemas, becase it's no more necessary

--- backend/utils/JoiErroSchema.js
+++
@@ -1,8 +0,0 @@
-const messages = {
-  'string.empty': 'Preencha todos os campos',
-  'number.positive': 'Campos numericos podem aceitar apenas inteiros positivos',
-  'number.base': 'Campos numericos so podem aceitar valores numericos',
-  'number.integer': 'Campo numerico somente aceita valores inteiros',
-};
-
-module.exports = messages;
