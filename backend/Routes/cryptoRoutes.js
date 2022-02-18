8c6f26e3ec47aba725e95b6ec3a79676bafdadc3
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-17 23:20:24 -0300

    Add middlewares to cryptoRoutes

--- backend/Routes/cryptoRoutes.js
+++ backend/Routes/cryptoRoutes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const cryptoController = require('../controllers/cryptoController');
+const {
+  verifyCurrencyValue,
+  verifyAllowedCurrency,
+} = require('../middlewares/cryptoValidation');
 
 router.get('/', cryptoController.getBtcCurrency);
-
+router.post('/', verifyAllowedCurrency, verifyCurrencyValue, cryptoController.updateCurrency);
 module.exports = router;
