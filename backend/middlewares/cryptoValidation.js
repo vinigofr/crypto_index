5f7debce9e856698c982b30044a08709eeeda6f4
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-17 23:19:49 -0300

    Add rules do validate currency and value - middlewares/cryptoValidation.js

--- backend/middlewares/cryptoValidation.js
+++ backend/middlewares/cryptoValidation.js
@@ -1,28 +1,39 @@
 const Joi = require('joi');
-const JoiSchemas = require('../utils/joiMessageSchema');
 
 // References:
 // https://joi.dev/api/?v=17.6.0
 // https://stackoverflow.com/questions/57993305/how-can-i-validate-number-of-digits-from-joi-using-nodejs
 const schema = Joi.object({
-  currency: Joi.string().required(),
   value: Joi.number().integer().positive().required(),
 });
 
-const updateCurrencyFieldsValidation = (req, res, next) => {
-  const { currency, value } = req.body;
-  const { error } = schema.validate({ currency, value });
+const verifyCurrencyValue = (req, res, next) => {
+  const { value } = req.body;
+  const { error } = schema.validate({ value });
 
   if (error) {
-    return res.status(400).json({
-      message: 'Campos inválidos',
-      details: JoiSchemas[error.details[0].type],
-    });
+    return res.status(400).json({ message: 'Valor inválido' });
+  }
+
+  return next();
+};
+
+const verifyAllowedCurrency = (req, res, next) => {
+  const { currency } = req.body;
+  const currencySchema = {
+    BRL: true,
+    EUR: true,
+    CAD: true,
+  };
+
+  if (!currencySchema[currency]) {
+    return res.status(400).json({ message: 'Moeda inválida' });
   }
 
   return next();
 };
 
 module.exports = {
-  updateCurrencyFieldsValidation,
+  verifyCurrencyValue,
+  verifyAllowedCurrency,
 };
