1a54d812c164c09cf1677e01c059dffa3c169013
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-17 21:02:55 -0300

    Add utils/joiMessageSchema.js

---
+++ backend/utils/joiMessageSchema.js
@@ -0,0 +1,8 @@
+const messages = {
+  'string.empty': 'Preencha todos os campos',
+  'number.positive': 'Campos numericos podem aceitar apenas inteiros positivos',
+  'number.base': 'Campos numericos so podem aceitar valores numericos',
+  'number.integer': 'Campo numerico somente aceita valores inteiros',
+};
+
+module.exports = messages;
