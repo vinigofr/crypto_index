ad06c117be1f06ecb116f0f7149d1626b10af9a8
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-18 12:58:28 -0300

    Add body syntax error handler

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,22 @@ const app = express();
 // Routes
 app.use(express.json());
 
+// Source: https://stackoverflow.com/questions/53048642/node-js-handle-body-parser-invalid-json-error/53049009
+// Handle syntax error from body
+app.use((err, req, res, next) => {
+  // This check makes sure this is a JSON parsing issue, but it might be
+  // coming from any middleware, not just body-parser:
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    return res.status(400).json({
+      error: {
+        message: 'Verifique o corpo da requisição',
+        details: 'Erro de sintaxe',
+      },
+    });
+  }
+  return next();
+});
+
 const helloWorldRoutes = require('./Routes/helloWorldRoutes');
 const loginRoutes = require('./Routes/loginRoutes');
 const cryptoRoutes = require('./Routes/cryptoRoutes');
