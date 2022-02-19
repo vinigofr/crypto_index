551aeb5d438918f1f2889274509d7b02b18de3df
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-19 13:55:41 -0300

    Add NotFound page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,11 @@ import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-// Components
+// Page components
 const Home = lazy(() => import('./Pages/Home'));
 const Login = lazy(() => import('./Pages/Login'));
 const Update = lazy(() => import('./Pages/Update'));
+const NotFound = lazy(() => import('./Pages/NotFound'));
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/update" element={<Update />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
     </Router>
