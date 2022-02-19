d6fbb84846bc10a369e5d1a0fc128943ee8fdfc5
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-19 18:04:10 -0300

    Fix bug: string no rate_float

--- backend/utils/formatCurrency.js
+++ backend/utils/formatCurrency.js
@@ -1,11 +1,11 @@
 // Source:
 // https://stackoverflow.com/questions/24758817/javascript-number-tolocalestring-with-4-digits-after-separator
-const getRateFloat = (currencyValue, usdRateFloat) => Number((
+const getRate = (currencyValue, usdRateFloat) => Number((
   Number(currencyValue) * usdRateFloat)
   .toFixed(4))
   .toLocaleString('en-US', { minimumFractionDigits: 4 });
 
-const getRate = (currencyValue, usdRateFloat) => Number((
+const getRateFloat = (currencyValue, usdRateFloat) => Number((
   Number(currencyValue) * usdRateFloat)
   .toFixed(4));
 
